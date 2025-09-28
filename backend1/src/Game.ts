97da09e61dfcd29e291dac81730ee0eb1f24b37f
Rutvik Shinde <shinderutviksandeep@gmail.com>
2025-09-28 12:50:40 +0530

    first commit

---
+++ backend1/src/Game.ts
@@ -0,0 +1,100 @@
+import { WebSocket } from "ws";
+import { Chess } from "chess.js";
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages.js";
+
+export class Game {
+  player1: WebSocket;
+  player2: WebSocket;
+  public board: Chess;
+  private startTime: Date;
+  private moveCount = 0;
+
+  constructor(player1: WebSocket, player2: WebSocket) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = new Chess();
+    this.startTime = new Date();
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "white",
+        },
+      })
+    );
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "black",
+        },
+      })
+    );
+  }
+
+  makeMove(socket: WebSocket, move: { from: string; to: string }) {
+    // validate the type of move using zod
+    if (this.moveCount % 2 === 0 && socket !== this.player1) {
+      return;
+    }
+    if (this.moveCount % 2 === 0 && socket !== this.player1) {
+      return;
+    }
+
+    console.log("did not early return");
+    console.log("move from", move.from, "move to", move.to);
+    try {
+      this.board.move(move);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+
+    console.log("made move");
+
+    if (this.board.isGameOver()) {
+      // send the game over message to both parties
+      this.player1.send(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            move,
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      this.player2.send(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            move,
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      console.log("game over sent");
+      return;
+    }
+
+    console.log(this.moveCount % 2);
+    if (this.moveCount % 2 === 0) {
+      console.log("sent1");
+      this.player2.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    } else {
+      console.log("sent2");
+      this.player1.send(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    }
+
+    this.moveCount++;
+  }
+}
---
+++ frontend/src/components/ChessBoard.tsx
@@ -0,0 +1,84 @@
+import type { Color, PieceSymbol, Square } from "chess.js";
+import { useState } from "react";
+import { MOVE } from "../screens/Game";
+
+export const ChessBoard = ({
+  board,
+  socket,
+  setBoard,
+  chess,
+}: {
+  board: ({
+    square: Square;
+    type: PieceSymbol;
+    color: Color;
+  } | null)[][];
+  socket: WebSocket;
+  setBoard: any;
+  chess: any;
+}) => {
+  const [from, setFrom] = useState<null | Square>(null);
+  const [to, setTo] = useState<null | Square>(null);
+
+  return (
+    <div className="text-white-200">
+      {board.map((row, i) => {
+        return (
+          <div key={i} className="flex">
+            {row.map((square, j) => {
+              const squareRepresentation = (String.fromCharCode(97 + (j % 8)) +
+                "" +
+                (8 - i)) as Square;
+              return (
+                <div
+                  onClick={() => {
+                    if (!from) {
+                      setFrom(squareRepresentation);
+                    } else {
+                      socket.send(
+                        JSON.stringify({
+                          type: MOVE,
+                          payload: {
+                            move: {
+                              from,
+                              to: squareRepresentation,
+                            },
+                          },
+                        })
+                      );
+                      setFrom(null);
+                      chess.move({
+                        from,
+                        to: squareRepresentation,
+                      });
+                      setBoard(chess.board());
+                    }
+                  }}
+                  key={j}
+                  className={`w-16 h-16 ${
+                    (i + j) % 2 === 0 ? "bg-green-500" : "bg-slate-500"
+                  }`}
+                >
+                  <div className="w-full justify-center flex h-full">
+                    <div className="h-full justify-center flex flex-col">
+                      {square ? (
+                        <img
+                          className="w-7"
+                          src={`/${
+                            square?.color === "b"
+                              ? square?.type
+                              : `${square?.type?.toUpperCase()} copy`
+                          }.png`}
+                        />
+                      ) : null}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
